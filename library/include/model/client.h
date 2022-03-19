0a889f8a596bc68e651dc2fc03274fa8eee5d67f
240365 Radkevich Pavel <240365@edu.p.lodz.pl>
2022-03-19 15:35:26 +0100

    Warsztat1 zrobiony

--- library/include/model/client.h
+++ library/include/model/client.h
@@ -4,11 +4,27 @@
 
 #ifndef CARRENTAL_CLIENT_H
 #define CARRENTAL_CLIENT_H
+namespace wypozyczalnia {
+    class client {
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string personalID;
 
+    public:
+        client(std::string initial_firstName, std::string initial_lastName, std::string initial_personalID);
+        ~client();
+        std::string getClientinfo ();
 
-class client {
+        //gettery
+        std::string get_firstName ();
+        std::string get_lastName ();
+        std::string get_personalID ();
 
-};
-
-
+        //settery
+        void set_firstName (std::string);
+        void set_lastName (std::string);
+        void set_personalID (std::string);
+    };
+}
 #endif //CARRENTAL_CLIENT_H
--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <math_helpers.h>
+#include "model/client.h"
 using namespace std;
 int main() {
 cout << "Hello World!"  << endl << factorial(5) << endl;
+wypozyczalnia::client h1 ("Leon", "Nowak", "1234");
+wypozyczalnia::client *h2 = new wypozyczalnia::client("Now", "LOt", "4567");
+
+
+cout << h1.getClientinfo() << endl;
+h1.set_firstName("");
+cout << h1.getClientinfo() << endl;
+//cout << h2->getClientinfo() << endl;
+delete h2;
 
 }
